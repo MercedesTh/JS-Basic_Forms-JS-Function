3949357901d6b57df1450f6476859d6e767a7550
Mercedes <mercedeswagner84@gmail.com>
2022-01-19 16:52:37 +0100

    title/added task 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,11 +8,22 @@ function calc() {
 }
 
 
-// Aufgabe 2 js_einführung_forms+ja-function-double_Lev1_2
+// Aufgabe 2 js_einführung_forms+ja-function-your-age_Lev1_2
 
 let year = document.getElementById("year");
 let howOld = document.getElementById("howOld");
 
 function age() {
     howOld.innerHTML = 2022 - year.value;
+}
+
+
+// Aufgabe 3 js_einführung_forms+ja-function-compare-age_Lev1_3
+
+let age1 = document.getElementById("age1");
+let age2 = document.getElementById("age2");
+let result = document.getElementById("result");
+
+function comp() {
+    compResult.innerHTML = age1.value - age2.value;
 }
